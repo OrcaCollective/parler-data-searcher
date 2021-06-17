4cbe91382ad512cd4849658c1d9268b5e6bd452e
sarayourfriend <24264157+sarayourfriend@users.noreply.github.com>
2021-06-16 21:28:55 -0700

    Make the full connection configurable

--- src/connection.py
+++ src/connection.py
@@ -3,9 +3,11 @@ import os
 
 MONGO_USER = os.environ.get("MONGO_USER")
 MONGO_PASS = os.environ.get("MONGO_PASS")
+MONGO_ENDPOINT = os.environ.get("MONGO_ENDPOINT")
+MONGO_PORT = os.environ.get("MONGO_PORT", "")
 
 client = MongoClient(
-    f"mongodb://{MONGO_USER}:{MONGO_PASS}@parler-data.tech-bloc-sea.dev", port=27017
+    f"mongodb://{MONGO_USER}:{MONGO_PASS}@{MONGO_ENDPOINT}", port=int(MONGO_PORT)
 )
 
 db = client.parler
