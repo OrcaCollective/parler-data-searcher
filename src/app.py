216f6f22ccab79b647dced50e725b5c40c3dd9ef
sarayourfriend <24264157+sarayourfriend@users.noreply.github.com>
2021-06-16 22:01:27 -0700

    Use Motor to connect to mongo

--- src/app.py
+++ src/app.py
@@ -1,12 +1,21 @@
 #!/usr/bin/env python
 from quart import Quart, render_template
+from quart_motor import Motor
 from bson import json_util
 
-from connection import db
-
 import json
+import os
+
+MONGO_USER = os.environ.get("MONGO_USER")
+MONGO_PASS = os.environ.get("MONGO_PASS")
+MONGO_ENDPOINT = os.environ.get("MONGO_ENDPOINT")
+MONGO_PORT = os.environ.get("MONGO_PORT")
 
 app = Quart(__name__, static_folder="public", template_folder="views")
+
+mongo = Motor(
+    app, uri=f"mongodb://{MONGO_USER}:{MONGO_PASS}@{MONGO_ENDPOINT}:{MONGO_PORT}/parler"
+)
 
 
 ################################################################################
@@ -19,12 +28,13 @@ async def home():
 
 @app.route("/posts")
 async def posts():
-    return json.dumps(db.posts.find_one(), default=json_util.default), 200
+    post = await mongo.db.posts.find_one()
+    return json.dumps(post, default=json_util.default), 200
 
 
 @app.route("/users")
 async def users():
-    return json.dumps(db.users.find_one(), default=json_util.default), 200
+    return json.dumps(await mongo.db.users.find_one(), default=json_util.default), 200
 
 
 if __name__ == "__main__":
