37fb1a8f6ec02bec3d390bd5fdce0d76115a34f2
sarayourfriend <24264157+sarayourfriend@users.noreply.github.com>
2021-06-17 16:52:07 -0700

    Add Post type

--- src/api_types.py
+++ src/api_types.py
@@ -1,7 +1,54 @@
-from typing import TypedDict
+from typing import Optional, TypedDict
 
 
 class User(TypedDict):
     name: str
     username: str
     avatar: str
+
+
+class PostMedia(TypedDict):
+    link: str
+    title: str
+    image: str
+    excerpt: str
+
+
+class PostComment(TypedDict):
+    username: str
+    date: str
+    text: str
+    replies: int
+    echos: int
+    upvotes: int
+
+
+class Echo(TypedDict):
+    username: str
+    date: str
+    text: str
+    impressions: int
+    image: Optional[str]
+    video: Optional[str]
+    media: Optional[PostMedia]
+    # Echos do have an echo property but it is always null
+    echo: None
+    comments: list[PostComment]
+    comment_count: int
+    echo_count: int
+    upvote_count: int
+
+
+class Post(TypedDict):
+    username: str
+    date: str
+    text: str
+    impressions: int
+    image: Optional[str]
+    video: Optional[str]
+    media: Optional[PostMedia]
+    echo: Optional[Echo]
+    comments: list[PostComment]
+    comment_count: int
+    echo_count: int
+    upvote_count: int
