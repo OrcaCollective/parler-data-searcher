7eb6400658f4140ea7feaeb1a7894a728a251927
sarayourfriend <24264157+sarayourfriend@users.noreply.github.com>
2021-06-18 13:26:56 -0700

    Add rudimentary posts view (#5)
    * Add Post type
    
    * Add rudimentary posts view
    
    * Fire off both db requests at the same time
    
    * Give more detail when logging the exception
    
    * Combine multiple with blocks
    
    Co-authored-by: Madison Swain-Bowden <bowdenm@spu.edu>
    
    Co-authored-by: Madison Swain-Bowden <bowdenm@spu.edu>

--- src/api.py
+++ src/api.py
@@ -4,8 +4,9 @@ from math import floor
 from pymongo.errors import OperationFailure
 
 import logging
+import asyncio
 
-from api_types import User
+from api_types import User, Post
 
 
 logger = logging.getLogger(__name__)
@@ -41,15 +42,73 @@ async def get_users(
     query = _get_users_query(search_term)
 
     try:
-        total_count: int = await mongo.db.users.count_documents(query)
-        results: list[User] = (
-            await mongo.db.users.find(query)
+        total_count_f = mongo.db.users.count_documents(query)
+        results_f = (
+            mongo.db.users.find(query)
             .skip(skip)
             .limit(PAGE_LIMIT)
             .to_list(length=PAGE_LIMIT)
         )
+        await asyncio.wait(
+            {total_count_f, results_f}, return_when=asyncio.FIRST_EXCEPTION
+        )
+        # indiscriminantly await each future, if one failed then the exception will raise as expected
+        total_count: int = await total_count_f
+        results: list[User] = await results_f
     except OperationFailure as err:
-        logger.error(err)
+        logger.error(f"Failure retrieving users: {err}")
+
+        # probably an invalid regex, just return nothing
+        total_count = 0
+        results = []
+
+    page_count = floor(total_count / PAGE_LIMIT) + 1
+
+    return page_count, results
+
+
+def _get_post_query(search_term: str) -> dict:
+    search_regex = {
+        "$regex": f".*{search_term}.*",
+        "$options": "i",
+    }
+    return {
+        "$or": [
+            {
+                "username": search_regex,
+            },
+            {
+                "comments.username": search_regex,
+            },
+            {
+                "echo.username": search_regex,
+            },
+        ],
+    }
+
+
+async def get_posts(
+    mongo: Motor, search_term: str, page: int
+) -> Tuple[int, list[Post]]:
+    skip = page * PAGE_LIMIT
+    query = _get_post_query(search_term)
+
+    try:
+        total_count_f = mongo.db.posts.count_documents(query)
+        results_f = (
+            mongo.db.posts.find(query)
+            .skip(skip)
+            .limit(PAGE_LIMIT)
+            .to_list(length=PAGE_LIMIT)
+        )
+        await asyncio.wait(
+            {total_count_f, results_f}, return_when=asyncio.FIRST_EXCEPTION
+        )
+        # indiscriminantly await each future, if one failed then the exception will raise as expected
+        total_count: int = await total_count_f
+        results: list[Post] = await results_f
+    except OperationFailure as err:
+        logger.error(f"Failure retrieving posts: {err}")
 
         # probably an invalid regex, just return nothing
         total_count = 0
