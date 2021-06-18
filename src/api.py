38e8b0309b1cb64150e0cd3b599142cf297d3133
sarayourfriend <24264157+sarayourfriend@users.noreply.github.com>
2021-06-18 06:07:49 -0700

    Fire off both db requests at the same time

--- src/api.py
+++ src/api.py
@@ -4,6 +4,7 @@ from math import floor
 from pymongo.errors import OperationFailure
 
 import logging
+import asyncio
 
 from api_types import User, Post
 
@@ -41,13 +42,19 @@ async def get_users(
     query = _get_users_query(search_term)
 
     try:
-        total_count: int = await mongo.db.users.count_documents(query)
-        results: list[User] = (
-            await mongo.db.users.find(query)
+        total_count_f = mongo.db.users.count_documents(query)
+        results_f = (
+            mongo.db.users.find(query)
             .skip(skip)
             .limit(PAGE_LIMIT)
             .to_list(length=PAGE_LIMIT)
         )
+        await asyncio.wait(
+            {total_count_f, results_f}, return_when=asyncio.FIRST_EXCEPTION
+        )
+        # indiscriminantly await each future, if one failed then the exception will raise as expected
+        total_count: int = await total_count_f
+        results: list[User] = await results_f
     except OperationFailure as err:
         logger.error(err)
 
@@ -87,13 +94,19 @@ async def get_posts(
     query = _get_post_query(search_term)
 
     try:
-        total_count: int = await mongo.db.posts.count_documents(query)
-        results: list[Post] = (
-            await mongo.db.posts.find(query)
+        total_count_f = mongo.db.posts.count_documents(query)
+        results_f = (
+            mongo.db.posts.find(query)
             .skip(skip)
             .limit(PAGE_LIMIT)
             .to_list(length=PAGE_LIMIT)
         )
+        await asyncio.wait(
+            {total_count_f, results_f}, return_when=asyncio.FIRST_EXCEPTION
+        )
+        # indiscriminantly await each future, if one failed then the exception will raise as expected
+        total_count: int = await total_count_f
+        results: list[Post] = await results_f
     except OperationFailure as err:
         logger.error(err)
 
