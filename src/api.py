11ea795253faab911c418784d08998e79478012f
sarayourfriend <24264157+sarayourfriend@users.noreply.github.com>
2021-06-20 16:30:10 -0700

    Add searching post content fields (#11)
    * Separate forms and search post content
    
    * Remove unused template
    
    * Move search type into links at top of page
    
    * Shorten the placeholders
    
    * Prevent accidental get-all search
    
    * Remove tabs
    
    * Rename search_term to username
    
    * Use `type="search"` for text inputs
    
    * Use urlencode to build query string
    
    * Exact match usernames and combine with content query

--- src/api.py
+++ src/api.py
@@ -1,5 +1,5 @@
 from quart_motor import Motor
-from typing import Tuple, Type, TypeVar
+from typing import Optional, Tuple, Type, TypeVar
 from math import floor
 from pymongo.errors import OperationFailure
 
@@ -15,9 +15,9 @@ logger = logging.getLogger(__name__)
 PAGE_LIMIT = 20
 
 
-def get_users_query(search_term: str) -> dict:
+def get_users_query(username: str) -> dict:
     search_regex = {
-        "$regex": f".*{search_term}.*",
+        "$regex": f".*{username}.*",
         "$options": "i",
     }
 
@@ -33,30 +33,57 @@ def get_users_query(search_term: str) -> dict:
     }
 
 
-def get_post_query(search_term: str) -> dict:
-    search_regex = {
-        "$regex": f".*{search_term}.*",
-        "$options": "i",
-    }
+def get_post_query(username: str, search_content: str) -> Optional[dict]:
+    username_query = {}
+    if username:
+        formatted_username = f"@{username}"
+        username_query = {
+            "$or": [
+                {
+                    "username": formatted_username,
+                },
+                {
+                    "comments.username": formatted_username,
+                },
+                {
+                    "echo.username": formatted_username,
+                },
+            ],
+        }
 
-    return {
-        "$or": [
-            {
-                "username": search_regex,
-            },
-            {
-                "comments.username": search_regex,
-            },
-            {
-                "echo.username": search_regex,
-            },
-        ],
-    }
+    content_query = {}
+    if search_content:
+        content_regex = {
+            "$regex": f".*{search_content}.*",
+            "$options": "i",
+        }
+        content_query = {
+            "$or": [
+                {
+                    "text": content_regex,
+                },
+            ],
+        }
+
+    # avoid an empty $or clause which will cause an error
+    if not username_query and not content_query:
+        return None
+
+    if username_query and not content_query:
+        return username_query
+
+    if content_query and not username_query:
+        return content_query
+
+    return {"$and": [username_query, content_query]}
 
 
 async def get_entities(
-    mongo: Motor, collection: str, query: dict, page: int, entity: Type[T]
+    mongo: Motor, collection: str, query: Optional[dict], page: int, entity: Type[T]
 ) -> Tuple[int, list[T]]:
+    if query is None:
+        return 0, []
+
     skip = page * PAGE_LIMIT
 
     try:
--- src/app.py
+++ src/app.py
@@ -3,6 +3,7 @@ from quart import Quart, render_template, request, redirect
 from quart_motor import Motor
 import os
 from dotenv import load_dotenv
+from urllib.parse import urlencode
 
 from api_types import User, Post
 import api
@@ -24,34 +25,42 @@ mongo = Motor(
 ################################################################################
 # Home page reroute
 ################################################################################
+ROUTE_PARAMS = {
+    "search_type",
+}
+
+
 @app.route("/")
 async def home():
-    search_term = request.args.get("search_term")
+    search_type = request.args.get("search_type")
 
-    if search_term is not None:
-        search_type = request.args.get("search_type", "users")
-        page = request.args.get("page", 0)
-        return redirect(f"/{search_type}/{search_term}?page={page}")
+    if search_type is not None:
+        query_params = urlencode(
+            {k: v for k, v in request.args.items() if k not in ROUTE_PARAMS}
+        )
+        return redirect(f"/{search_type}?{query_params}")
 
-    return await render_template("index.html")
+    # by default just render the users search
+    return await render_template("users.html")
 
 
-@app.route("/posts", defaults={"search_term": ""}, strict_slashes=False)
-@app.route("/posts/<search_term>")
-async def posts(search_term):
+@app.route("/posts", strict_slashes=False)
+async def posts():
+    username = request.args.get("username", "")
+    search_content = request.args.get("search_content", "")
     page = request.args.get("page", 0)
     try:
         page = int(page)
     except ValueError:
         page = 0
 
-    if not search_term:
-        return await render_template("index.html")
+    if not username and not search_content:
+        return await render_template("posts.html")
 
     page_count, results = await api.get_entities(
         mongo,
         "posts",
-        api.get_post_query(search_term),
+        api.get_post_query(username, search_content),
         page,
         Post,
     )
@@ -60,28 +69,29 @@ async def posts(search_term):
         "posts.html",
         posts=results,
         page=page,
-        search_term=search_term,
+        username=username,
+        search_content=search_content,
         page_count=page_count,
         search_type="posts",
     )
 
 
-@app.route("/users", defaults={"search_term": ""}, strict_slashes=False)
-@app.route("/users/<search_term>")
-async def users(search_term):
+@app.route("/users", strict_slashes=False)
+async def users():
+    username = request.args.get("username")
     page = request.args.get("page", 0)
     try:
         page = int(page)
     except ValueError:
         page = 0
 
-    if not search_term:
-        return await render_template("index.html")
+    if not username:
+        return await render_template("users.html")
 
     page_count, results = await api.get_entities(
         mongo,
         "users",
-        api.get_users_query(search_term),
+        api.get_users_query(username),
         page,
         User,
     )
@@ -90,7 +100,7 @@ async def users(search_term):
         "users.html",
         users=results,
         page=page,
-        search_term=search_term,
+        username=username,
         page_count=page_count,
         search_type="users",
     )
