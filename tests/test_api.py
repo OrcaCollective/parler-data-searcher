21e3572fd3b8b4faee7f92d531040e1907c3df40
sarayourfriend <24264157+sarayourfriend@users.noreply.github.com>
2021-06-20 17:01:32 -0700

    Add some unit tests

---
+++ tests/test_api.py
@@ -0,0 +1,73 @@
+from src import api
+
+
+def test_get_post_query_returns_None():
+    assert api.get_post_query("", "") is None
+
+
+def test_get_post_query_returns_only_username_query():
+    test_username = "@test-username"
+
+    assert api.get_post_query("test-username", "") == {
+        "$or": [
+            {
+                "username": test_username,
+            },
+            {
+                "comments.username": test_username,
+            },
+            {
+                "echo.username": test_username,
+            },
+        ],
+    }
+
+
+def test_get_post_query_returns_only_content_query():
+    test_content_search = "test content search"
+    content_regex = {
+        "$regex": f".*{test_content_search}.*",
+        "$options": "i",
+    }
+
+    assert api.get_post_query("", test_content_search) == {
+        "$or": [
+            {
+                "text": content_regex,
+            },
+        ],
+    }
+
+
+def test_get_post_query_returns_full_query():
+    test_username = "@test-username"
+    test_content_search = "test content search"
+    content_regex = {
+        "$regex": f".*{test_content_search}.*",
+        "$options": "i",
+    }
+
+    assert api.get_post_query("test-username", test_content_search) == {
+        "$and": [
+            {
+                "$or": [
+                    {
+                        "username": test_username,
+                    },
+                    {
+                        "comments.username": test_username,
+                    },
+                    {
+                        "echo.username": test_username,
+                    },
+                ],
+            },
+            {
+                "$or": [
+                    {
+                        "text": content_regex,
+                    },
+                ],
+            },
+        ],
+    }
