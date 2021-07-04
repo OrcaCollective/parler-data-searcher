f4063ad24a8989f000ac6d9adbde445e3d954e62
sarayourfriend <24264157+sarayourfriend@users.noreply.github.com>
2021-07-04 08:49:52 -0700

    Just use Mock

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,7 +1,7 @@
 import pytest
 import re
 from unittest.mock import patch
-from unittest.mock import MagicMock
+from unittest.mock import Mock
 from quart_motor import Motor
 
 import api
@@ -12,7 +12,7 @@ from enums import SearchBehavior
 @patch("api._get_entities")
 async def test_search_posts_with_mentions(get_entities):
     get_entities.return_value = 0, []
-    mongo = MagicMock(spec=Motor)
+    mongo = Mock(spec=Motor)
     username = "@test_username"
     content = "test_content"
 
